b02d4d877b916e32d0b84c6254100afe63aae0d3
chris chen <christinachen149@gmail.com>
2022-11-29 01:23:17 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./assets/bootstrap/all.scss";
+import "bootstrap";
+
+createApp(App).mount('#app')
